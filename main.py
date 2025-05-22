e6c353c0c91bb0261d4e6a97d3eb0d3fea932e85
Alexandra Lache <alelache@localhost.localdomain>
2025-05-22 12:40:38 +0300

    Add implementation until now

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas.plotting import table
 import sys
 import labelsDict
 
-HEART_DATASET_PATH     = 'datasets/heart_4_train.csv'
-PIRVISION_DATASET_PATH = 'datasets/pirvision_office_train.csv'
+HEART_DATASET_PATH         = 'datasets/heart_4_train.csv'
+PIRVISION_DATASET_PATH     = 'datasets/pirvision_office_train.csv'
+HEART_DESCRIPTION_PATH     = 'description/heart.csv'
+PIRVISION_DESCRIPTION_PATH = 'description/pirvision.csv'
 
-def plot_boxplot(dataset : pd.DataFrame, continue_values : list):
+def plot_boxplot_value_range(dataset : pd.DataFrame, continue_values : list, outputname: str):
     boxprops = dict(linestyle='-', linewidth=1.5, color='darkblue', facecolor='lightblue')
 
     plt.figure(figsize=(18, 6))
@@ -22,34 +25,44 @@ def plot_boxplot(dataset : pd.DataFrame, continue_values : list):
     plt.yscale('log')
     plt.tight_layout()
     plt.show()
+    # plt.savefig(outputname)
+
+def plot_description(characteristic : pd.Series, outputname: str):
+    plot = plt.subplot(111, frame_on=False)
+
+    plot.xaxis.set_visible(False) 
+    plot.yaxis.set_visible(False)
+    table(plot, characteristic, loc='upper right')
+
+    plt.show()
+    # plt.savefig(outputname)
 
 
 def main():
     filename = sys.argv[1]
     dictLabled = {}
+    description_csv = ''
 
     if filename == HEART_DATASET_PATH:
         dictLabled = labelsDict.HeartDisease
+        description_csv = HEART_DESCRIPTION_PATH
     elif filename == PIRVISION_DATASET_PATH:
         dictLabled = labelsDict.Pirvision
+        description_csv = PIRVISION_DESCRIPTION_PATH
     else:
         print("Unknown set. Please label columns beforehand")
         return -1
     
     dataset = pd.read_csv(filename)
 
-    # if filename == PIRVISION_DATASET_PATH:
-    #     dataset['Timestamp'] = pd.to_datetime(dataset['Timestamp'], errors='coerce')
-    #     dataset['Day'] = pd.to_datetime(dataset['Day'], errors='coerce')
-    #     print(dataset['Day'])
-    #     print(dataset['Timestamp'])
-
     continue_values = [col for col in dataset if dictLabled[col]==labelsDict.CONTINUE]
     discrete_values = [col for col in dataset if dictLabled[col]==labelsDict.DISCRETE]
 
-    for value in continue_values:
-        print(dataset[value].describe())
-    
+    # Save description for continue features
+    dataset[continue_values].describe().T.reset_index().to_csv(description_csv, index = False)
+
+    # Save description for discrete features
+
    
 
 if __name__=='__main__':
