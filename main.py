3101ae49d9d5165f27cf23e5174af6b3b8b36039
Lache Alexandra Florentina Georgiana <alexandralache0802@yahoo.com>
2025-05-25 00:06:29 +0300

    Add first implementation of DecisionTreeClassifier

--- main.py
+++ main.py
@@ -3,21 +3,58 @@ import pandas as pd
 from pandas.plotting import table
 import seaborn as sns
 import sys
+import numpy as np
+from labelsDict import HeartDisease, Pirvision
 from heart_dataset_eda import heart_eda_statistics
 from pirvision_dataset_eda import pirvision_eda_statistics
-from heart_dataset_preprocessing import heart_preprocessing
-from pirvision_dataset_preprocessing import pirvision_preprocessing
+from preprocessing_dataset import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.utils.class_weight import compute_class_weight
+
+
+HEART_DATASET_PATH_TRAIN        = 'datasets/heart_4_train.csv'
+HEART_DATASET_PATH_TEST         = 'datasets/heart_4_test.csv'
+PIRVISION_DATASET_PATH_TRAIN    = 'datasets/pirvision_office_train.csv'
+PIRVISION_DATASET_PATH_TEST     = 'datasets/pirvision_office_test.csv'
 
 def main():
     index = sys.argv[1]
-
     
     if index == '0':
         # heart_eda_statistics()
-        heart_preprocessing(imputation='mean')
+        X_train, X_test, y_train, y_test = preprocessing(HeartDisease,
+                                                          train_path=HEART_DATASET_PATH_TRAIN, test_path=HEART_DATASET_PATH_TEST,
+                                                          imputation='median', quantile1=0.25, quantile3=0.75,
+                                                          scaler_type='standard', correlation_factor=0.9)
     elif index == '1':
         # pirvision_eda_statistics()
-        pirvision_preprocessing(imputation='mean')
+        X_train, X_test, y_train, y_test = preprocessing(Pirvision, train_path=PIRVISION_DATASET_PATH_TRAIN, test_path=PIRVISION_DATASET_PATH_TEST, imputation='mean')
+
+    print(np.unique(y_train))
+    print(np.unique(y_train, return_counts=True))
+
+    classes = np.unique(y_train)
+
+    class_weights = compute_class_weight(class_weight='balanced', classes=classes, y=y_train.values.reshape(-1))
+    class_weight_dict = dict(zip(classes, class_weights))
+
+    print(class_weight_dict)
+
+    clf = DecisionTreeClassifier(
+        max_depth=10,
+        min_samples_leaf=5,
+        criterion='gini',
+        class_weight=class_weight_dict
+    )
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+
 
 if __name__=='__main__':
     main()
--- preprocessing_dataset.py
+++ preprocessing_dataset.py
@@ -2,20 +2,17 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 import numpy as np
-from labelsDict import CONTINUE, HeartDisease
-# from sklearn.impute import IterativeImputer
-# from sklearn.experimental import enable_iterative_imputer 
+from labelsDict import CONTINUE, DISCRETE, LABEL
+from sklearn.experimental import enable_iterative_imputer 
+from sklearn.impute import IterativeImputer
 
-HEART_DATASET_PATH_TRAIN    = 'datasets/heart_4_train.csv'
-HEART_DATASET_PATH_TEST     = 'datasets/heart_4_test.csv'
 
-def heart_preprocessing(imputation:str='mean', quantile1:float=0.25, quantile3:float=0.75, scaler_type:str='standard', correlation_factor:float=0.9) -> tuple[pd.DataFrame, pd.DataFrame]:
-    df_train = pd.read_csv(HEART_DATASET_PATH_TRAIN)
-    df_test = pd.read_csv(HEART_DATASET_PATH_TEST)
-
+def preprocessing(dictionary:dict, train_path:str, test_path:str, imputation:str='mean', quantile1:float=0.25, quantile3:float=0.75, scaler_type:str='standard', correlation_factor:float=0.9) -> tuple[pd.DataFrame, pd.DataFrame]:
+    df_train = pd.read_csv(train_path)
+    df_test = pd.read_csv(test_path)
     # Replacing outliers with Nan
     for col in df_train:
-        if HeartDisease[col] != CONTINUE:
+        if dictionary[col] != CONTINUE:
             continue
         Q1 = df_train[col].quantile(quantile1)
         Q3 = df_train[col].quantile(quantile3)
@@ -33,27 +30,26 @@ def heart_preprocessing(imputation:str='mean', quantile1:float=0.25, quantile3:f
         imputer = SimpleImputer(strategy='median')
     elif imputation == 'most_frequent':
         imputer = SimpleImputer(strategy='most_frequent')
-    # elif imputation == 'iterative':
-    #     imputer = IterativeImputer()
+    elif imputation == 'iterative':
+        imputer = IterativeImputer()
     else:
         raise ValueError(f"Unknown imputation method: {imputation}")
 
-    numeric_cols = df_train.select_dtypes(include=['float64', 'int64']).columns
-    # print(numeric_cols)
-    # print(df_train.columns)
+    columns = [cols for cols in df_train if dictionary[cols] in [DISCRETE, CONTINUE, LABEL]] 
 
-    df_train[numeric_cols] = imputer.fit_transform(df_train[numeric_cols])
-    df_test[numeric_cols]  = imputer.transform(df_test[numeric_cols])
+    df_train[columns] = imputer.fit_transform(df_train[columns])
+    df_test[columns]  = imputer.transform(df_test[columns])
 
-    corr_matrix = df_train[numeric_cols].corr().abs()
+    columns = [cols for cols in df_train if dictionary[cols] in [DISCRETE, CONTINUE]]
+
+    corr_matrix = df_train[columns].corr().abs()
     upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
     to_drop = [column for column in upper.columns if any(upper[column] > correlation_factor)]
-    # print(to_drop)
 
     df_train = df_train.drop(columns=to_drop)
     df_test = df_test.drop(columns=to_drop)
 
-    numeric_cols = df_train.select_dtypes(include=['float64', 'int64']).columns
+    numeric_cols = [cols for cols in df_train.columns if dictionary[cols] in [DISCRETE, CONTINUE]]
 
     if scaler_type == 'standard':
         scaler = StandardScaler()
@@ -67,6 +63,8 @@ def heart_preprocessing(imputation:str='mean', quantile1:float=0.25, quantile3:f
     df_train[numeric_cols] = scaler.fit_transform(df_train[numeric_cols])
     df_test[numeric_cols]  = scaler.transform(df_test[numeric_cols])
 
-    
-    return df_train, df_test
+    columns = [cols for cols in df_train.columns if dictionary[cols] in [DISCRETE, CONTINUE]]
+    labels  = [cols for cols in df_train.columns if dictionary[cols] in LABEL]
+
+    return df_train[columns], df_test[columns], df_train[labels], df_test[labels]
 
