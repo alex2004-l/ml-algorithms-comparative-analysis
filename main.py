aded38f8fc7912b632898e28d1c9f645f049a901
Lache Alexandra Florentina Georgiana <alexandralache0802@yahoo.com>
2025-05-25 12:09:03 +0300

    Add correct logistic regresion and confusion matrix plotting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pandas.plotting import table
 import seaborn as sns
 import sys
 import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from labelsDict import HeartDisease, Pirvision
 from heart_dataset_eda import heart_eda_statistics
 from pirvision_dataset_eda import pirvision_eda_statistics
@@ -14,15 +15,23 @@ from sklearn.utils.class_weight import compute_class_weight
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.preprocessing import LabelEncoder
 
 HEART_DATASET_PATH_TRAIN        = 'datasets/heart_4_train.csv'
 HEART_DATASET_PATH_TEST         = 'datasets/heart_4_test.csv'
 PIRVISION_DATASET_PATH_TRAIN    = 'datasets/pirvision_office_train.csv'
 PIRVISION_DATASET_PATH_TEST     = 'datasets/pirvision_office_test.csv'
 
-def decision_tree_classifier(X_train, X_test, y_train):
-    class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train.values.reshape(-1))
+def plot_confusion_matrix(cm, outputname = None):
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot(cmap='Reds')
+    if outputname:
+        plt.savefig(outputname, dpi = 100)
+    plt.show()
+
+
+def decision_tree_classifier(X_train, X_test, y_train, y_test):
+    class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
     class_weight_dict = dict(zip(np.unique(y_train), class_weights))
 
     clf = DecisionTreeClassifier(
@@ -36,10 +45,16 @@ def decision_tree_classifier(X_train, X_test, y_train):
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
 
-    return y_pred
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-def random_forest_classifier(X_train, X_test, y_train):
-    class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train.values.reshape(-1))
+    confusion = confusion_matrix(y_test, y_pred)
+    plot_confusion_matrix(confusion, "confusion/decision_tree_heart.png")
+
+
+
+def random_forest_classifier(X_train, X_test, y_train, y_test):
+    class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
     class_weight_dict = dict(zip(np.unique(y_train), class_weights))
 
     rf_model = RandomForestClassifier(
@@ -55,10 +70,13 @@ def random_forest_classifier(X_train, X_test, y_train):
 
     rf_model.fit(X_train, y_train)
     y_pred = rf_model.predict(X_test)
+    
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-    return y_pred
 
-def mlp_classifier(X_train, X_test, y_train):
+
+def mlp_classifier(X_train, X_test, y_train, y_test):
     mlp = MLPClassifier(
         hidden_layer_sizes=(128, 64),
         activation='relu',
@@ -75,56 +93,55 @@ def mlp_classifier(X_train, X_test, y_train):
     mlp.fit(X_train, y_train)
 
     y_pred = mlp.predict(X_test)
-    return y_pred
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+
+def logistic_regression_classifier(X1, X2, y_train, y_test):
+    X_train = X1.values
+    X_test  = X2.values
+    X_train = np.hstack([np.ones((X1.shape[0], 1)), X1])
+    X_test = np.hstack([np.ones((X2.shape[0], 1)), X2])
+
+    unique_classes = np.unique(y_train)
+
+    prob_matrix = np.zeros((X_test.shape[0], len(unique_classes)))
+
+    for y_class in unique_classes:
+        y_encoded_train_modified = (y_train==y_class).astype(int)
+        w = train_and_eval_logistic(X_train, y_encoded_train_modified, lr = 0.01, epochs_no=10000)
+        y_pred_probs = predict_logistic(X_test, w)
+        prob_matrix[:, y_class] = y_pred_probs
+
+
+    y_pred = np.argmax(prob_matrix, axis=1)
+
+    print("Accuracy:",accuracy_score(y_pred, y_test))
+    print(classification_report(y_test, y_pred))
+
 
 def logistic(x):
     return 1 / (1 + np.exp(-x))
 
-def nll(Y, T):
-    N = T.shape[0]
-    return -1/N * np.sum(T * np.log(Y) + (1-T) * np.log(1 - Y))
-
 def predict_logistic(X, w):
-    if isinstance(X, pd.DataFrame):
-        X = X.to_numpy()
     return logistic(X @ w)
 
-def accuracy(Y, T):
-    Y = np.array(Y).flatten()
-    T = np.array(T).flatten()
-    return np.mean((Y >= 0.5) == T)
-
-def train_and_eval_logistic(X_train: pd.DataFrame, T_train: pd.Series,
-                            X_test: pd.DataFrame, T_test: pd.Series,
+def train_and_eval_logistic(X_train, T_train,
                             lr=0.01, epochs_no=100):
-    X_train = X_train.to_numpy()
-    X_test = X_test.to_numpy()
-    T_train = T_train.to_numpy().flatten()
-    T_test = T_test.to_numpy().flatten()
 
     N_train, D = X_train.shape
     w = np.random.randn(D)
 
-    train_acc, test_acc = [], []
-    train_nll, test_nll = [], []
-
     for epoch in range(epochs_no):
         Y_train = predict_logistic(X_train, w)
-        Y_test = predict_logistic(X_test, w)
-
-        train_acc.append(accuracy(Y_train, T_train))
-        test_acc.append(accuracy(Y_test, T_test))
-        train_nll.append(nll(Y_train, T_train))
-        test_nll.append(nll(Y_test, T_test))
-
         grad = (1 / N_train) * X_train.T @ (Y_train - T_train)
         w = w - lr * grad
-
-    return w, train_nll, test_nll, train_acc, test_acc
+    return w
 
 
 def main():
     index = sys.argv[1]
+    ml_type = sys.argv[2]
     
     if index == '0':
         # heart_eda_statistics()
@@ -136,40 +153,28 @@ def main():
         # pirvision_eda_statistics()
         X_train, X_test, y_train, y_test = preprocessing(Pirvision,
                                                          train_path=PIRVISION_DATASET_PATH_TRAIN, test_path=PIRVISION_DATASET_PATH_TEST,
-                                                         imputation='mean', quantile1=0.1, quantile3=0.9,
+                                                         imputation='median', quantile1=0.1, quantile3=0.9,
                                                           scaler_type='standard', correlation_factor=0.9)
 
-    y_pred = decision_tree_classifier(X_train, X_test, y_train)
 
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+    label_encoder = LabelEncoder()
 
-    y_pred = random_forest_classifier(X_train, X_test, y_train)
+    y_train = np.array(y_train).ravel()
+    y_test = np.array(y_test).ravel()
 
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+    y_train = label_encoder.fit_transform(y_train)
+    y_test  = label_encoder.transform(y_test)
 
-    y_pred = mlp_classifier(X_train, X_test, y_train)
-
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
-
-    w, train_nll, test_nll, train_acc, test_acc = train_and_eval_logistic(X_train, y_train, X_test, y_test, lr=0.1, epochs_no=10000)
-
-    y_pred_probs = predict_logistic(X_test, w)
-    y_pred = (y_pred_probs >= 0.5).astype(int)
-
-    print("Predicted labels:", np.unique(y_pred, return_counts=True))
-
-    print("Accuracy:",accuracy_score(y_pred, y_test))
-    print(classification_report(y_test, y_pred))
-
-    clf = LogisticRegression()
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+    if ml_type =='0':
+        decision_tree_classifier(X_train, X_test, y_train, y_test)
+    elif ml_type == '1':
+        random_forest_classifier(X_train, X_test, y_train, y_test)
+    elif ml_type == '3':
+        mlp_classifier(X_train, X_test, y_train, y_test)
+    elif ml_type == '2':
+        logistic_regression_classifier(X_train, X_test, y_train, y_test)
+    else:
+        raise ValueError("Unknown second argument")
 
 
 
